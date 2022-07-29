34053c7d9831e5cd352ee293e6f75a474cfa243b
coronado03 <sebascoronado03@hotmail.com>
2022-07-28 21:19:43 -0600

    first 2 pages basic

--- components/natalia.js
+++ components/natalia.js
@@ -1,8 +1,49 @@
 export default function Natalia() {
     return (  
       <>
-        <div className="bg-[#000001]">
+        <div className="bg-[#000001] grid w-full h-screen">
 
+            <div className="relative place-self-center">
+                <img 
+                className="max-h-[450px]"
+                src="/natalia/card2.png" 
+                alt="card"/>
+
+                    <div className="absolute left-[5px] top-[-14px]">
+                        <img
+                        className="max-h-[450px]"
+                        src='/natalia/card.png'
+                        alt='me!'/>
+
+                        <div className="absolute right-14 bottom-4">
+                            <img
+                            className="max-h-[510px]"
+                            src='/natalia/natalia.png'
+                            alt='me!'/>
+
+                        </div>
+
+                        <div className="absolute left-[85px] top-36">
+                            <img
+                            className="max-h-[130px]"
+                            src='/natalia/natalianame.png'
+                            alt='my name!'/>
+
+                        </div>
+
+                    </div>
+
+            
+            
+            
+            </div>
+
+
+
+
+
+            
+            
 
         </div>
       </>
